a08ab2d5b3a3ac2101a2dc57894aead08b375e6a
Emily Simpson <emlysimpson@gmail.com>
2020-01-14 16:08:42 -0600

    added models for db, seed file

--- server/db/models/index.js
+++ server/db/models/index.js
@@ -1,18 +1,20 @@
 const User = require('./user')
+const Product = require('./product')
+const Order = require('./order')
+const CartItems = require('./cart_items')
 
-/**
- * If we had any associations to make, this would be a great place to put them!
- * ex. if we had another model called BlogPost, we might say:
- *
- *    BlogPost.belongsTo(User)
- */
+Product.hasMany(CartItems)
+User.hasMany(CartItems)
+CartItems.belongsTo(User)
+CartItems.belongsTo(Product)
+CartItems.belongsTo(Order)
+Order.hasMany(CartItems)
 
-/**
- * We'll export all of our models here, so that any time a module needs a model,
- * we can just require it from 'db/models'
- * for example, we can say: const {User} = require('../db/models')
- * instead of: const User = require('../db/models/user')
- */
+Order.belongsTo(User)
+
 module.exports = {
-  User
+  User,
+  Product,
+  Order,
+  CartItems
 }
