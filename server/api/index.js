5dd0d9daa1309033a488cc2a81d02a6beb7faace
Murad Zalov <mzalov@gmail.com>
2020-01-15 12:03:25 -0600

    Added allProducts view to React component

--- server/api/index.js
+++ server/api/index.js
@@ -1,7 +1,11 @@
 const router = require('express').Router()
 module.exports = router
 
+// Users router
 router.use('/users', require('./users'))
+
+// // All Products router
+router.use('/products', require('./allProducts'))
 
 router.use((req, res, next) => {
   const error = new Error('Not Found')
