8eb9fcf5225519a201eace98297e6dee0648921f
Michael Yonkman <michaelyonkman@gmail.com>
2020-01-17 12:55:22 -0600

    purchase complete rendering on successful charge

--- client/components/CheckoutForm.js
+++ client/components/CheckoutForm.js
@@ -5,6 +5,7 @@ import axios from 'axios'
 class CheckoutForm extends Component {
   constructor(props) {
     super(props)
+    this.state = {complete: false}
     this.submit = this.submit.bind(this)
   }
 
@@ -16,10 +17,12 @@ class CheckoutForm extends Component {
     let response = await axios.post('/api/checkout', token)
     console.log(response)
 
-    if (response.ok) console.log('Purchase Complete!')
+    if (response.data.status === 'succeeded') this.setState({complete: true})
   }
 
   render() {
+    if (this.state.complete) return <h1>Purchase Complete</h1>
+
     return (
       <div className="checkout">
         <p>Would you like to complete the purchase?</p>
